47a834144e8c63a8e1bac134ab102eb5388c89d1
nahid787 <nahih787@gmail.com>
2022-09-04 20:22:31 +0600

    day 1

---
+++ day1/basicAdminTask2.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+# Author: Morshed Hasan
+# Date: sep 4, 2022
+# Modification date: sep 4, 2022
+# Description: This script will run basic system check commands.
+
+echo
+top
+echo
+df -h
+echo
+uptime
+echo
+iostat
+echo
+echo End of Script
---
+++ day1/basic_AdminTask1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#description: This shell script show your hostname, user id, present working directory and date
+# Date: sep 4, 20222
+# Modification date: sep 4, 20222
+
+echo
+hostname
+echo
+whoami
+echo
+pwd
+date
+echo
